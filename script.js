20576db8fad23cbd889edb4a83f4aa6b0f943867
louraridi <louaridiismail66@gmail.com>
2025-10-05 00:52:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Petit effet pour le menu : scroll smooth
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
